6321dc268fb433d00f2f2b3c68f5d67aff9540af
yangweiyi <yangweiyi@corp.netease.com>
2021-08-17 18:34:56 +0800

    初始化XLua

--- Assets/Scripts/CSLifetime.cs
+++ Assets/Scripts/CSLifetime.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LuaTesting : MonoBehaviour
+public class CSLifetime : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    void Awake()
+    {
+        Debug.Log("awake");
     }
 
     // Update is called once per frame
